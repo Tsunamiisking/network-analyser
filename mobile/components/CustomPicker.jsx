440754f3357f1cd1d0090d6b961e7c6ed5bfbb6a
Tsunamiisking <allenissabigboi@gmail.com>
2026-03-13 20:55:22 +0100

    Update font style for labels in CustomPicker and RadioButtonGroup components

--- mobile/components/CustomPicker.jsx
+++ mobile/components/CustomPicker.jsx
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
   },
   
   label: {
-    fontFamily: FONTS.medium,
+    fontFamily: FONTS.headerSemibold,
     fontSize: FONT_SIZES.body,
     color: COLORS.textPrimary,
     marginBottom: SPACING.sm,
