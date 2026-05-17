48467871411ea4a8e231396a2d7b5f7a7ac1e64b
Tsunamiisking <allenissabigboi@gmail.com>
2026-05-18 00:42:55 +0100

    feat: update API base URL to localhost for local development

--- backend/src/controllers/analyticsController.js
+++ backend/src/controllers/analyticsController.js
@@ -2,18 +2,33 @@ const NetworkData = require("../models/NetworkData");
 
 exports.providerComparison = async (req, res) => {
   try {
+    const PROVIDER_CANONICAL = { mtn: 'MTN', airtel: 'Airtel', glo: 'Glo', '9mobile': '9mobile' };
+
     const result = await NetworkData.aggregate([
       {
+        $addFields: { _providerKey: { $toLower: '$provider' } }
+      },
+      {
         $group: {
-          _id: "$provider",
-          averageSignal: { $avg: "$signalStrength" },
+          _id: '$_providerKey',
+          averageSignal: { $avg: '$signalStrength' },
           totalSamples: { $sum: 1 }
         }
       },
       {
         $project: {
-          provider: "$_id",
-          averageSignal: { $round: ["$averageSignal", 2] },
+          provider: {
+            $switch: {
+              branches: [
+                { case: { $eq: ['$_id', 'mtn'] },     then: 'MTN'     },
+                { case: { $eq: ['$_id', 'airtel'] },  then: 'Airtel'  },
+                { case: { $eq: ['$_id', 'glo'] },     then: 'Glo'     },
+                { case: { $eq: ['$_id', '9mobile'] }, then: '9mobile' },
+              ],
+              default: '$_id'
+            }
+          },
+          averageSignal: { $round: ['$averageSignal', 2] },
           totalSamples: 1,
           _id: 0
         }
@@ -49,25 +64,38 @@ exports.blackoutRate = async (req, res) => {
     const result = await NetworkData.aggregate([
       { $match: matchStage },
       {
+        $addFields: { _providerKey: { $toLower: '$provider' } }
+      },
+      {
         $group: {
-          _id: "$provider",
+          _id: '$_providerKey',
           totalSamples: { $sum: 1 },
           blackoutSamples: {
-            $sum: { $cond: [{ $eq: ["$connectivityFlag", false] }, 1, 0] }
+            $sum: { $cond: [{ $eq: ['$connectivityFlag', false] }, 1, 0] }
           },
-          averageSignal: { $avg: "$signalStrength" },
+          averageSignal: { $avg: '$signalStrength' },
         }
       },
       {
         $project: {
           _id: 0,
-          provider: "$_id",
+          provider: {
+            $switch: {
+              branches: [
+                { case: { $eq: ['$_id', 'mtn'] },     then: 'MTN'     },
+                { case: { $eq: ['$_id', 'airtel'] },  then: 'Airtel'  },
+                { case: { $eq: ['$_id', 'glo'] },     then: 'Glo'     },
+                { case: { $eq: ['$_id', '9mobile'] }, then: '9mobile' },
+              ],
+              default: '$_id'
+            }
+          },
           totalSamples: 1,
           blackoutSamples: 1,
-          averageSignal: { $round: ["$averageSignal", 2] },
+          averageSignal: { $round: ['$averageSignal', 2] },
           blackoutRate: {
             $round: [
-              { $multiply: [{ $divide: ["$blackoutSamples", "$totalSamples"] }, 100] },
+              { $multiply: [{ $divide: ['$blackoutSamples', '$totalSamples'] }, 100] },
               2
             ]
           }
--- web-app/src/config/api.js
+++ web-app/src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://172.20.10.4:3000/api';
+export const API_BASE_URL = 'http://localhost:3000/api';
 
 export const PROVIDERS = ['MTN', 'Airtel', 'Glo', '9mobile'];
 
